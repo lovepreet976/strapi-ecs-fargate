96c8c01afd5bfa33f555bfde757f271b45905876
Lovepreet Singh <95311620+lovepreet976@users.noreply.github.com>
2025-12-18 17:05:30 +0530

    CI/CD Trigger

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,16 +1,17 @@
-resource "aws_ecr_repository" "strapi" {
-  name = "application-deployment"
+# resource "aws_ecr_repository" "strapi" {
+ # name = "application-deployment"
 
-  image_scanning_configuration {
-    scan_on_push = true
-  }
+  #image_scanning_configuration {
+#    scan_on_push = true
 
-  tags = {
-    Project = "strapi-ecs"
-    Managed = "terraform"
-  }
+#  }
 
-  lifecycle {
-    prevent_destroy = true
-  }
-}
+#  tags = {
+#    Project = "strapi-ecs"
+#    Managed = "terraform"
+#  }
+
+#  lifecycle {
+#    prevent_destroy = true
+#  }
+# }
